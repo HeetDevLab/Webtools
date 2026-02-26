4425138f1892c083019ec32181d7c35d5fd5be7c
HeetdevLab <155948193+HeetDevLab@users.noreply.github.com>
2026-02-26 14:13:27 +0530

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,40 @@
+const CACHE_NAME = "heetdevlab-webtools-v1";
+
+const urlsToCache = [
+  "/Webtools/",
+  "/Webtools/index.html",
+  "/Webtools/style.css",
+  "/Webtools/icon-192.png",
+  "/Webtools/icon-512.png"
+];
+
+// Install
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+// Activate
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+});
+
+// Fetch
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
