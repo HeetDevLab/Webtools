36da59ff73b29e41bf667ec547f6b1a0c95e90e6
HeetdevLab <155948193+HeetDevLab@users.noreply.github.com>
2026-02-27 11:58:58 +0530

    Update particles-config.js

--- particles-config.js
+++ particles-config.js
@@ -1,23 +1,27 @@
-particlesJS("particles-js", {
-  particles: {
-    number: { value: 55 },
-    size: { value: 2 },
-    color: { value: "#2ea8ff" },
-    line_linked: {
-      enable: true,
-      distance: 130,
-      color: "#2ea8ff",
-      opacity: 0.2,
-      width: 1
-    },
-    move: {
-      enable: true,
-      speed: 1.2
-    }
-  },
-  interactivity: {
-    events: {
-      onhover: { enable: true, mode: "grab" }
-    }
+document.addEventListener("DOMContentLoaded", function () {
+  if (document.getElementById("particles-js")) {
+    particlesJS("particles-js", {
+      particles: {
+        number: { value: 55 },
+        size: { value: 2 },
+        color: { value: "#2ea8ff" },
+        line_linked: {
+          enable: true,
+          distance: 130,
+          color: "#2ea8ff",
+          opacity: 0.2,
+          width: 1
+        },
+        move: {
+          enable: true,
+          speed: 1.2
+        }
+      },
+      interactivity: {
+        events: {
+          onhover: { enable: true, mode: "grab" }
+        }
+      }
+    });
   }
 });
